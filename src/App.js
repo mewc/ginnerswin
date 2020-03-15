81ee1546a062ed4fab0951310a4e5ac4ae104cc5
Michael Carter <m@mewc.info>
2020-03-15 17:52:11 +1100

    update with links

--- src/App.js
+++ src/App.js
@@ -7,25 +7,38 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://instagram.com/ginners_win/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Instagram
-        </a>
+        <div>
+          <a
+            className="App-link linkylink"
+            href="https://apps.apple.com/au/app/ginners-netball/id1474243413"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            iOS App
+          </a>
+          <a
+            className="App-link linkylink"
+            href="https://hiscores.ginners.win"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Hiscores
+          </a>
+        </div>
+        <br />
 
-        <a
-          className="App-link"
-          href="https://apps.apple.com/au/app/ginners-netball/id1474243413"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          GinnersNetball App 
+        <div>
+          <a
+            className="App-link"
+            href="https://instagram.com/ginners_win/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Instagram
         </a>
+        </div>
       </header>
-    </div>
+    </div >
   );
 }
 
